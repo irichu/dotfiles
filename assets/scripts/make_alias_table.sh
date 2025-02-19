30a7ebb351ddce6210562a304cf157e208b93def
irichu <197713379+irichu@users.noreply.github.com>
2025-02-19 22:37:04 +0900

    👻 escape markdown talbe contents

--- assets/scripts/make_alias_table.sh
+++ assets/scripts/make_alias_table.sh
@@ -6,5 +6,5 @@ if [ -z "$1" ]; then
 fi
 
 # show alias table with csview
-rg -v '^\s*#' "$1" | rg -o 'alias .+=.+' | cut -d' ' -f2- | sed "s/=/\t/" | sed "s/'//g" | sed "s/|/\&#124;/g" | csview --tsv --no-headers --style=markdown
+rg -v '^\s*#' "$1" | rg -o 'alias .+=.+' | cut -d' ' -f2- | sed "s/=/\t/" | sed "s/'/\`/g" | csview --tsv --no-headers --style=markdown
 
