c062d9a1b6c32e4b141913bfbae398b14ad0bf2f
irichu <197713379+irichu@users.noreply.github.com>
2025-02-19 22:23:32 +0900

    📑 add materials of zsh alias table

---
+++ assets/scripts/make_alias_table.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo 'please specify a file or directory'
+  exit 1
+fi
+
+# show alias table with csview
+rg -v '^\s*#' "$1" | rg -o 'alias .+=.+' | cut -d' ' -f2- | sed "s/=/\t/" | sed "s/'//g" | csview --tsv --no-headers --style=markdown
+
