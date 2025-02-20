bce2875df5ce3d5cf94ce6ad7caeaada33469b39
irichu <197713379+irichu@users.noreply.github.com>
2025-02-20 12:04:54 +0900

    🗒️ update alias list

--- assets/scripts/make_alias_table.sh
+++ assets/scripts/make_alias_table.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+set -ue
+set -o pipefail
+
+export LC_ALL=C
+
+# check arg
 if [ -z "$1" ]; then
   echo 'please specify a file or directory'
   exit 1
 fi
 
-# show alias table with csview
-rg -v '^\s*#' "$1" | rg -o 'alias .+=.+' | cut -d' ' -f2- | sed "s/=/\t/" | sed "s/'/\`/g" | csview --tsv --no-headers --style=markdown
+# markdown table header
+echo '| alias | entity |'
+echo '| :- | :- |'
 
+# print alias table with csview
+rg -v '^\s*#' "$1" | rg -o 'alias .+=.+' | cut -d' ' -f2- | sed "s/=/\t/" | sed "s/|/\&#124;/g" | sed "s/'/\`/g" | csview --tsv --no-headers --style=markdown
