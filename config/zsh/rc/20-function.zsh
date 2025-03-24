8e71e3e670904fe5f1b0d3d44829cef3d3720073
irichu <197713379+irichu@users.noreply.github.com>
2025-03-24 12:10:24 +0900

    ✨ feat: Enhance zsh function

--- config/zsh/rc/20-function.zsh
+++ config/zsh/rc/20-function.zsh
@@ -1,8 +1,33 @@
+#!/usr/bin/env zsh
+
 # functions
 
 # utils
-function cdls() { \cd $1 && ls -l }
-function mkdircd() { mkdir -p $1 && cd $_ }
+function cdls() {
+  \cd $1 && ls -l
+}
+
+function mkdircd() {
+  mkdir -p $1 && cd $_
+}
+
+function killport() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: killport <port>"
+    return 1
+  fi
+
+  local pids
+  pids=$(lsof -i:"$1" -t)
+
+  if [[ -z "$pids" ]]; then
+    echo "No process found on port $1"
+    return 1
+  fi
+
+  echo "Killing processes on port $1: $pids"
+  echo "$pids" | xargs kill -9
+}
 
 # tmux
 function tvim() {
@@ -15,21 +40,22 @@ function tvim() {
 }
 
 function confirm {
-  echo -n "$1 [y/N]: "; read -q; return $?
+  echo -n "$1 [y/N]: "
+  read -q
+  return $?
 }
 
 # yazi shell wrappers
 function y() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		builtin cd -- "$cwd"
-	fi
-	rm -f -- "$tmp"
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  yazi "$@" --cwd-file="$tmp"
+  if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    builtin cd -- "$cwd"
+  fi
+  rm -f -- "$tmp"
 }
 
 # bat
 help() {
-    "$@" --help 2>&1 | bathelp
+  "$@" --help 2>&1 | bathelp
 }
-
