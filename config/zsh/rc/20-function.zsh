3fe13fd7b44992e81daa0eb19febf5c9c6358d01
irichu <197713379+irichu@users.noreply.github.com>
2025-03-24 18:10:03 +0900

    ✨ feat: Add new environment variable aliases and functions

--- config/zsh/rc/20-function.zsh
+++ config/zsh/rc/20-function.zsh
@@ -29,6 +29,44 @@ function killport() {
   echo "$pids" | xargs kill -9
 }
 
+function print_xdg_env() {
+  local xdg_envs=(
+    XDG_CACHE_HOME
+    XDG_CONFIG_DIRS
+    XDG_CONFIG_HOME
+    XDG_CURRENT_DESKTOP
+    XDG_DATA_DIRS
+    XDG_DATA_HOME
+    XDG_MENU_PREFIX
+    XDG_RUNTIME_DIR
+    XDG_SESSION_CLASS
+    XDG_SESSION_DESKTOP
+    XDG_SESSION_TYPE
+    XDG_STATE_HOME
+  )
+
+  for env in ${xdg_envs[@]}; do
+    echo "$env=$(printenv $env)"
+  done
+}
+
+function print_proxy_env() {
+  local proxy_envs=(
+    HTTPS_PROXY
+    HTTP_PROXY
+    FTP_PROXY
+    NO_PROXY
+    https_proxy
+    http_proxy
+    ftp_proxy
+    no_proxy
+  )
+
+  for env in ${proxy_envs[@]}; do
+    echo "$env=$(printenv $env)"
+  done
+}
+
 # tmux
 function tvim() {
   tmux split-window -v
