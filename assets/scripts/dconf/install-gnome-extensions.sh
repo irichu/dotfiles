814790c86c8a294f79c262057aaf66c3f50da9e9
irichu <197713379+irichu@users.noreply.github.com>
2025-06-18 22:35:09 +0900

    🔨 refactor: Refactor gnome extensions installation

--- assets/scripts/dconf/install-gnome-extensions.sh
+++ assets/scripts/dconf/install-gnome-extensions.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 # Install gnome extensions
-gext install AlphabeticalAppGrid@stuarthayhurst
-gext install blur-my-shell@aunetx
-gext install compiz-alike-magic-lamp-effect@hermes83.github.com
-gext install compiz-windows-effect@hermes83.github.com
-gext install just-perfection-desktop@just-perfection
-gext install space-bar@luchrioh
-gext install tactile@lundal.io
-gext install tophat@fflewddur.github.io
-gext install undecorate@sun.wxg@gmail.com
+EXT_IDS=(
+  AlphabeticalAppGrid@stuarthayhurst
+  blur-my-shell@aunetx
+  compiz-alike-magic-lamp-effect@hermes83.github.com
+  compiz-windows-effect@hermes83.github.com
+  just-perfection-desktop@just-perfection
+  space-bar@luchrioh
+  tactile@lundal.io
+  tophat@fflewddur.github.io
+  undecorate@sun.wxg@gmail.com
+)
+
+# Install gnome-extensions-cli
+if [ ! -f "$HOME/.local/bin/gext" ]; then
+  pip install gnome-extensions-cli
+fi
+
+# Install and enable extensions
+for ext in "${EXT_IDS[@]}"; do
+  "$HOME/.local/bin/"gext install "$ext"
+  gnome-extensions enable "$ext"
+done
+
+# Ubuntu Dock
+gnome-extensions enable ubuntu-dock@ubuntu.com
