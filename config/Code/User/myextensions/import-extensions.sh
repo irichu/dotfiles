a5799f41fdaf0066551db9e932f83a2639444702
irichu <197713379+irichu@users.noreply.github.com>
2025-06-22 12:14:11 +0900

    🔨 refactor: Update script paths

--- config/Code/User/myextensions/import-extensions.sh
+++ config/Code/User/myextensions/import-extensions.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-cat code-extensions.txt | while read extension; do echo "Install $extension ..." && code --install-extension "$extension"; done
+SCRIPT_DIR=$(
+  cd "$(dirname "${BASH_SOURCE:-$0}")"
+  pwd
+)
+
+cat "$SCRIPT_DIR/code-extensions.txt" | while read extension; do echo "Install $extension ..." && code --install-extension "$extension"; done
